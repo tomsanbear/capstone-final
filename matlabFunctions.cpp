902a702347738e45ed263345236cb9cdeb768945
John Doe <tsanterre3@gmail.com>
2016-03-26 13:57:08 -0400

    fixed lda computation...kinda...TODO

--- matlabFunctions.cpp
+++ matlabFunctions.cpp
@@ -5,15 +5,26 @@ float distCompare(float *first, float *second){
 	return 1;
 }
 
-void ldaComputation(std::vector<float> weights,std::vector<User> &masterList){
+void ldaComputation(std::vector<float> &weights,std::vector<User> &masterList){
 	long int npoints = masterList.size();
 	long int nvars = masterList.size();
-	long int nclasses = 3;
-	long int info = 1;
+	long int nclasses = 2;
+	long int info = 0;
 	alglib::real_1d_array w;
+	int rows = 2;
+	int cols = 2;
+
+	// We initilize this variable with the proper rows and columns
 	alglib::real_2d_array xy;
+	xy.setlength(masterList[1].vectorCoefs.size(), masterList.size());
 	// Load masterlist into the xy array
-	alglib::fisherlda(&xy,npoints,nvars,nclasses,&info,&w);
+	for(int i = 0; i <= rows; i++){
+		for(int j=0;j<=cols;j++){
+			xy(i,j)=masterList[j].vectorCoefs[i];
+		}
+	}
+	//Execute the algorithm to obtain training weights
+	alglib::fisherlda(xy,npoints,nvars,nclasses,info,w);
 	//Send weights back to original function
 	return;
 }
